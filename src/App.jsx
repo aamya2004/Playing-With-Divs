85e4e74fa61f23580cb0c8b8d3b4642d8ad58679
aamya2004 <93673911+aamya2004@users.noreply.github.com>
2023-11-14 19:01:09 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   const [isClicked, setIsClicked] = useState(null);
   const divs = [1, 2, 3, 4];
 
+  const [isClickedEye, setIsClickedEye] = useState(false);
+
   const handleClick = (id) => {
     setIsClicked(id);
   };
@@ -21,6 +23,13 @@ const App = () => {
             </div>
           </div>
         ))}
+      </div>
+      <div className="h-[100px] w-[500px] bg-gray-400 m-10 rounded-2xl flex items-center justify-center gap-2 ">
+        <input type={isClickedEye ? "text" : "password"} placeholder="Enter your password..." className= "rounded-lg"></input>
+        {
+          isClickedEye ? <img onClick={()=> setIsClickedEye(false)} src="src\assets\hide.png" className="w-[20px] h-[20px]"></img>
+          :<img onClick={()=> setIsClickedEye(true)} src="src\assets\view.png" className="w-[20px] h-[20px]"></img>
+        }
       </div>
     </>
   );
